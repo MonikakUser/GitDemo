a6e9e4d8b09a850eeb3453936b726c078c4aacf0
Monika <monika.khandelwal07@gmail.com>
2024-05-08 16:35:56 +0530

    commit 4

--- src/Hello.java
+++ src/Hello.java
@@ -7,11 +7,7 @@ public class Hello {
 		System.out.println("hello");	
 		System.out.println("hello1");	
 		System.out.println("hello2");
-		System.out.println("hello3");	
-		System.out.println("hello4");	
-		System.out.println("hello5");	
-		System.out.println("hello");	
-		System.out.println("hello1");	
+		
 		System.out.println("hello2");
 		System.out.println("hello3");	
 		System.out.println("hello4");	
